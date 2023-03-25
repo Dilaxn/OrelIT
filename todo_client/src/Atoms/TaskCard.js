03f5c2a656348399f35664ffc94aff9077016f0f
Dilaxn <dilaxn@gmail.com>
2023-03-25 14:56:43 +0530

    new task add , task delete, task get and update done for frontend level have to check and start to build backend

---
+++ todo_client/src/Atoms/TaskCard.js
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {Checkbox, FormControl, FormHelperText, Grid, Input, InputLabel} from "@material-ui/core";
+import {useEffect, useState} from "react";
+import IconButton from '@mui/material/IconButton';
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from '@mui/icons-material/Edit';
+
+
+export default function TaskCard(props) {
+
+    useEffect(() => {
+       setCompleted(props.completed)
+    }, []);
+const done = props.completed
+    const [editIndex, setEditIndex] = useState(-1);
+    const [completed, setCompleted] = useState(props.completed);
+
+    function handleEditClick() {
+        console.log("Edit clicked for task: ", props.task);
+        console.log("Task card clicked!", props.index);
+        setEditIndex(props.index);
+    }
+
+    function editFinished() {
+        console.log("Edit clicked for task: ", props.task);
+        console.log("Task card clicked!", props.index);
+        setEditIndex(-1);
+    }
+
+    function handleComplete() {
+        console.log("Edit clicked for task: ", props.task);
+        console.log("Task card clicked!", props.index);
+        setCompleted(!completed);
+    }
+
+    function handleChange(event) {
+
+        // console.log("selected event : ", event.target.value);
+    }
+
+    return (
+        <Card style={{marginTop:20}}>
+            <CardContent style={{textAlign:'center'}}>
+                {editIndex === props.index ? (
+                    <Grid container>
+
+                    <Grid item xs={9} style={{textAlign:'left'}}>
+                        <FormControl fullWidth>
+                            <InputLabel htmlFor="edit-task">Edit Task</InputLabel>
+                            <Input id="edit-task" defaultValue={props.task} />
+                            {/*<FormHelperText>Click outside the box to save changes</FormHelperText>*/}
+                        </FormControl>
+                    </Grid>
+                        <Grid item xs={3}>
+                            <Typography gutterBottom variant="h5" component="div" style={{textDecoration: completed ? 'line-through' : 'none'}}>
+                                <IconButton onClick={editFinished}>
+                                    <CheckIcon />
+                                </IconButton>
+                            </Typography>
+
+                        </Grid>
+                    </Grid>
+                    ) :(
+                <Grid container>
+                    <Grid item xs={3}>
+                        <IconButton onClick={props.onClick}>
+                            <DeleteIcon />
+                        </IconButton>
+                        <IconButton onClick={handleEditClick}>
+                            <EditIcon />
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={6}>
+                <Typography gutterBottom variant="h5" component="div" style={{textDecoration: completed ? 'line-through' : 'none'}}>
+                    {props.task}
+                </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Typography gutterBottom variant="h5" component="div" style={{textDecoration: done ? 'line-through' : 'none'}}>
+                            <IconButton onClick={handleComplete}>
+                                <CheckIcon style={{color:completed ? 'red' : 'grey'}}/>
+                            </IconButton>
+                        </Typography>
+
+                    </Grid>
+                </Grid>
+                    )}
+            </CardContent>
+        </Card>
+    );
+}
--- todo_client/src/Home.js
+++ todo_client/src/Home.js
@@ -7,57 +7,74 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {FormControl, FormHelperText, Grid, InputLabel, TextField} from "@mui/material";
 import {Input} from "@mui/icons-material";
+import TaskCard from "./Atoms/TaskCard";
+import data from './MockData/tasks.json';
+import {useEffect, useState} from "react";
 
 export default function Home() {
+const [taskCollection,setTaskCollection] = useState(data)
+    const [newTask, setNewTask] = useState("");
+
+    // useEffect(() => {
+    //     setTaskCollection(data);
+    // }, []);
+
+    function handleClick(id) {
+        console.log("Task card clicked!", id);
+        const newTasks = taskCollection.tasks.filter((task) => task.id !== id);
+        setTaskCollection({ tasks: newTasks }); // update the task collection state
+        console.log(newTasks);
+    }
+    function handleClick2(index) {
+        console.log("Task card 222 clicked!", index);
+        const newTasks = [...taskCollection.tasks]; // create a new array copy
+        newTasks.splice(index, 1); // remove the task at the specified index
+        setTaskCollection({ tasks: newTasks }); // update the task collection state
+    }
+
+    // Function for handle new task
+    function handleAddTask() {
+        const newTaskObject = {
+            content: newTask,
+            completed: false
+        };
+        setTaskCollection(prevState => {
+            return { tasks: [...prevState.tasks, newTaskObject] };
+        });
+        setNewTask("");
+    }
+
     return (
         <Grid container>
         <Grid item xs={12} md={2} xl={3}/>
         <Grid item xs={12} md={8} xl={6}>
 
 
-        <Card sx={{ maxWidth: 1000 }}>
+        <Card sx={{ maxWidth: 1000 }} style={{marginTop:100, marginBottom:50}}>
 
             <CardContent style={{textAlign:'center'}}>
+                <Card >
+                <CardContent style={{textAlign:'center'}}>
                 <Typography gutterBottom variant="h3" component="div">
                     Wednesday, 7th Nov
                 </Typography>
+                    </CardContent>
+                </Card>
                 <Typography gutterBottom variant="h6" component="div">
                     3 tasks
                 </Typography>
-                <Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-                <Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography><Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-                <Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography><Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-                <Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography><Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-                <Typography gutterBottom variant="h5" component="div">
-                    with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
+
+                {/*Here I map the all tasks with reusable component taskCard */}
+
+                {taskCollection &&  taskCollection.tasks.map((task, index) => (
+                    <TaskCard key={task.id} task={task.content} completed={task.completed}  onClick={() => handleClick(task.id)} onClick2={() => handleClick2(task.id)}/>
+                ))}
+
 
             </CardContent>
-            <CardActions style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <TextField style={{ display: 'block' }} id="standard-basic" label="Add todo" variant="standard" />
-                <Button  style={{ display: 'block', borderRadius: '50%',border:1, borderWidth:1, backgroundColor:"#ec042b", color:"white" }} size="large" >+</Button>
+            <CardActions style={{ justifyContent: 'center', alignItems: 'center', padding:30 }}>
+                <TextField fullWidth style={{ display: 'block' }} id="standard-basic" label="Add todo" variant="standard" onChange={(event) => setNewTask(event.target.value)} />
+                <Button  style={{ display: 'block', borderRadius: '50%',border:1, borderWidth:1, backgroundColor:"#ec042b", color:"white" }} onClick={handleAddTask} size="large" >+</Button>
 
             </CardActions>
         </Card>
